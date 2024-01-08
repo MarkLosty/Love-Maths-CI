d39ecca7c5111d5609be4cde72171035be6eec69
ml <ml123@gmail.com>
2024-01-08 13:05:56 +0000

    Initial project commit

---
+++ notes.js
@@ -0,0 +1,7 @@
+// Learning Outcomes
+// 1. Understand how JS interacts with the DOM
+// 2. Learn how to use Event Listeners to wait for user interaction
+// 3. Learn how to avoid creating global variables
+// 4. Comfortable with JS Functions
+
+// 
